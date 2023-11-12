a0ccc86cbb8c0e27c6093eaecb62b8e5189d8fa1
Danila Voronkov <danilavoronkov2002@gmail.com>
2023-11-12 19:01:47 +0300

    feat(user): add ability to delete by id

--- src/routes/user/user.controller.ts
+++ src/routes/user/user.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param, UseGuards } from '@nestjs/common';
 import { UserService } from '@/routes/user/user.service';
+import { User } from '@/routes/user/users.decorator';
+import { UserDto } from '@/routes/auth/dto/user.dto';
+import { MongoDbIdDto } from '@/shared/dto/mongoDbId.dto';
+import { AuthGuard } from '@/routes/auth/guards/auth.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Delete('/delete/:id')
+  @UseGuards(AuthGuard)
+  async deleteUser(@Param() params: MongoDbIdDto, @User() user: UserDto) {
+    return this.userService.deleteUser(params.id, user?.id);
+  }
 }
